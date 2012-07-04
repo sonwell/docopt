fba91d2b385028d251b542728e2b16d3eb53c645
Andrew Kassen <atkassen@gmail.com>
2012-07-03 20:33:06 -0700

    simple example with nested commands, arguments, an (argument-less) option

--- nfa.py
+++ nfa.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+
+"""
+TODO:
+    1. Graph.get chooses the correct type e.g. Command, Option, Argument
+    2. Options
+"""
+
 class NodeValue(object):
 
     def __init__(self):
@@ -15,11 +23,11 @@ class NodeValue(object):
         return repr(self.value)
 
 
-class Epsilon(object):
+class Node(object):
 
-    def __init__(self):
+    def __init__(self, name=None):
         self.reprd = False
-        self.name = None
+        self.name = name
         self.next = []
         self.value = NodeValue()
 
@@ -56,11 +64,78 @@ class Epsilon(object):
         return repl
 
 
-class Node(Epsilon):
+class Graph(Node):
 
-    def __init__(self, name):
-        Epsilon.__init__(self)
-        self.name = name
+    def __init__(self, name=None, parent=None):
+        Node.__init__(self, name)
+        self.symbols = {} if parent is None else parent.symbols
+        self.internal = Node()
+        self.result = []
+
+    def get(self, symbol):
+        if symbol in self.symbols:
+            old = self.symbols[symbol]
+            new = old.__class__(symbol)
+            new.value = old.value
+            return new
+        if symbol.startswith('-'):
+            if symbol[1] == '-':
+                new = Option(None, symbol, [])
+            else:
+                new = Option(symbol, None, [])
+        elif symbol.isupper() or \
+           (symbol[0] == '<' and symbol[-1] == '>'):
+            new = Argument(symbol)
+        else:
+            new = Command(symbol)
+        self.symbols[symbol] = new
+        return new
+
+    def extend(self, tokens, parents):
+        parents.insert(0, self)
+        tail = self.internal.extend(tokens, parents)
+        if self not in tail.next:
+            tail.next.append(self)
+        tail.next.append(Node())
+        parents.pop(0)
+        return self
+
+    def exit(self, tokens, parents):
+        parents.pop(0)
+        res = Node.match(self, tokens, parents)
+        if res is not None:
+            return res
+        parents.insert(0, self)
+        return None
+        
+    def entry(self, tokens, parents):
+        if self.name:
+            try:
+                tok = tokens.pop(0)
+            except:
+                return None
+            if self != tok:
+                tokens.insert(0, tok)
+                return None
+        parents.insert(0, self)
+        return self.internal.match(tokens, parents)
+
+    def match(self, tokens, parents):
+        if parents and parents[0] is self:  # we've been here before...
+            return self.exit(tokens, parents)
+        else:
+            return self.entry(tokens, parents)
+
+    def __repr__(self):
+        start = str(self.internal).strip().split('\n  ')
+        end = (Node.__repr__(self)).split('\n  ')
+        repl = "<%s(%s)->%r @ %x>:\n  %s\n  %s\n" % \
+            (self.__class__.__name__, self.name, self.value,
+             id(self.value), '\n   |'.join(start), '\n    '.join(end))
+        return repl
+
+
+class Argument(Node):
 
     def extend(self, tokens, parents):
         try:
@@ -70,14 +145,14 @@ class Node(Epsilon):
         if self.name != tok:
             tokens.insert(0, tok)
             return None
-        return Epsilon.extend(self, tokens, parents)
+        return Node.extend(self, tokens, parents)
 
     def match(self, tokens, parents):
         try:
             tok = tokens.pop(0)
         except:
             return None
-        res = Epsilon.match(self, tokens, parents)
+        res = Node.match(self, tokens, parents)
         if res is not None:
             self.push(tok)
             res[self.name] = self.value
@@ -86,112 +161,86 @@ class Node(Epsilon):
         return None
 
 
-class Graph(Node):
+class Option(Graph):
 
-    def __init__(self, parent=None):
-        Node.__init__(self, None)
-        self.symbols = {} if parent is None else parent.symbols
-        self.start = Epsilon()
-        self.end = Epsilon()
-        self.next = self.end.next
+    def __init__(self, short, long, args):
+        Graph.__init__(self, long or short)
+        self.long = long
+        self.short = short
+        self.args = args
 
-    def get(self, symbol):
-        if symbol in self.symbols:
-            old = self.symbols[symbol]
-            new = old.__class__(symbol)
-            new.value = old.value
-            return new
-        new = Node(symbol)
-        self.symbols[symbol] = new
-        return new
-
-    def extend(self, tokens, parents):
-        parents.insert(0, self)
-        tail = self.start.extend(tokens, parents)
-        if self not in tail.next:
-            tail.next.append(self)
-        tail.next.append(Epsilon())
-        parents.pop(0)
-        return self.end
-
-    def match(self, tokens, parents):
-        if parents and parents[0] is self:  # we've been here before...
-            parents.pop(0)
-            res = self.end.match(tokens, parents)
-            if res is not None:
-                return res
-            parents.insert(0, self)
-            return None
-        else:
-            parents.insert(0, self)
-            return self.start.match(tokens, parents)
-
-    def __repr__(self):
-        start = str(self.start).strip().split('\n  ')
-        end = str(self.end).split('\n  ')
-        repl = "<%s(%s)->%r @ %x>:\n  %s\n  %s\n" % \
-            (self.__class__.__name__, self.name, self.value,
-             id(self.value), '\n   |'.join(start), '\n    '.join(end))
-        return repl
-
-
-class Argument(Node):
-    pass
-
-
-class Command(Graph):
-    
-    def __init__(self, name):
-        Graph.__init__(self, None)
-        self.name = name
-        self.result = []
+    def  __ne__(self, token):
+        return token not in (self.short, self.long)
 
     def extend(self, tokens, parents):
         try:
             tok = tokens.pop(0)
         except:
             return self
-        if self.name != tok:
+        if tok not in (self.long, self.short):
             tokens.insert(0, tok)
             return None
-        Graph.extend(self, tokens, parents)
+        tail = self.internal
+        for arg in self.args:
+            new = Argument(arg)
+            tail.next.append(new)
+            tail = new
+        tail.next.append(self)
+        return self
 
-    def match(self, tokens, parents):
-        if parents and parents[0] is self:
-            parents.pop(0)
-            res = self.end.match(tokens, parents)
-            if res is not None:
-                self.result.insert(0, res)
-                return {}
-            parents.insert(0, self)
+    def exit(self, tokens, parents):
+        parents.pop(0)
+        res = Node.match(self, tokens, parents)
+        if res is not None:
+            self.result.insert(0, res)
+            return {}
+        parents.insert(0, self)
+        return None
+
+    def entry(self, tokens, parents):
+        res = Graph.entry(self, tokens, parents)
+        if res is not None:
+            if self.args:
+                for arg in self.args:
+                    self.value.push(res[arg])
+            else:
+                self.value.push(True)
+            result = self.result.pop(0)
+            result[self.name] = self.value
+            return result
+        return None
+
+
+class Command(Option):
+    
+    def __init__(self, name):
+        Graph.__init__(self, name, None)
+
+    def __ne__(self, token):
+        return self.name != token
+
+    def extend(self, tokens, parents):
+        try:
+            tok = tokens.pop(0)
+        except:
+            return self
+        if self != tok:
+            tokens.insert(0, tok)
             return None
-        else:
-            if self.name is not None:
-                tok = tokens.pop(0)
-                if tok != self.name:
-                    tokens.insert(0, tok)
-                    return None
-            parents.insert(0, self)
-            res = self.start.match(tokens, parents)
-            if res is not None and self.result is not None:
-                result = self.result.pop(0)
-                result[self.name] = res
-                return result
-            return None
+        return Graph.extend(self, tokens, parents)
 
-
-class Option(Graph):
-    pass
+    def entry(self, tokens, parents):
+        res = Graph.entry(self, tokens, parents)
+        if res is not None:
+            result = self.result.pop(0)
+            result[self.name] = res
+            return result
+        return None
 
 
 if __name__ == '__main__':
     import sys
-    A = Command('rm')
-    A.extend('rm C C'.split(), [])
-    A.extend('rm C B'.split(), [])
+    A = Command('sudo')
+    A.extend('sudo rm C B -v'.split(), [])
 
-    B = Command('sudo')
-    A.next.append(B)
-    B.start.next.append(A)
-
-    print B.match(sys.argv[1:], [])
+    print A.match(sys.argv[1:], [])
